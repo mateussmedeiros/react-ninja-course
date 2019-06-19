7afde39bfdd7002718583d263502f79c16cfb3bf
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2019-06-18 21:11:42 -0300

    [ADD] Exemplo de Props

---
+++ m01/props/src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React from 'react'
+import Title from './title.js'
+
+const App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Title name='Mateus Medeiros' />
+      </div>
+    )
+  }
+})
+
+export default App
