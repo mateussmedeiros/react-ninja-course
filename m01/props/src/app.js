c25eeec4a889372d59e91fad3ea5e04391b8b03e
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2019-06-18 21:25:26 -0300

    [UPDATE] Atributos HTML

--- m01/props/src/app.js
+++ m01/props/src/app.js
@@ -6,8 +6,10 @@ import Title from './title.js'
 const App = React.createClass({
   render: function () {
     return (
-      <div>
+      <div className='container'>
         <Title name='Mateus Medeiros' />
+        <label htmlFor='input'>Input</label>
+        <input type='text' id='input' />
       </div>
     )
   }
