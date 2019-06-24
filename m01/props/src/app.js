900a85d332d1f6ba4cc13d1b72c19da7c3577f86
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2019-06-24 10:54:50 -0300

    [UPDATE] GetDefaultProps

--- m01/props/src/app.js
+++ m01/props/src/app.js
@@ -7,9 +7,7 @@ const App = React.createClass({
   render: function () {
     return (
       <div className='container'>
-        <Title name='Mateus Medeiros' />
-        <label htmlFor='input'>Input</label>
-        <input type='text' id='input' />
+        <Title />
       </div>
     )
   }
