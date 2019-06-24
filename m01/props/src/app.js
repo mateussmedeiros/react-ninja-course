8e96c8accff93c4ab5f26615aefbef9b3a8032d9
Mateus Medeiros <mateus.sousamedeiros@gmail.com>
2019-06-24 11:40:10 -0300

    [ADD] Exemplo com a Prop Key

--- m01/props/src/app.js
+++ m01/props/src/app.js
@@ -1,28 +1,18 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Title from './title'
+import Square from './square'
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
-        <Title name='Mateus' lastname='Medeiros' />
+        {['blue', 'red', 'green'].map((square) => (
+          <Square key={square} color={square} />
+        ))}
       </div>
     )
   }
 }
-
-/*
-const App = React.createClass({
-  render: function () {
-    return (
-      <div className='container'>
-        <Title name='Mateus' lastname='Medeiros' />
-      </div>
-    )
-  }
-})
-*/
 
 export default App
